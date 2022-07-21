f8b4f7511d6be5ab09ea677ad45636c7234d53f0
gruponichele <lfsc09@gmail.com>
2022-07-21 18:50:14 -0300

    0.0.0.3

--- src/components/back/NavSide/index.js
+++ src/components/back/NavSide/index.js
@@ -1,18 +1,89 @@
 import styles from './navside.module.css';
-import { CandlestickChart } from '@mui/icons-material';
-import { Button, Divider, Paper, Box } from '@mui/material';
-import React from 'react';
+import { Analytics, CandlestickChart, Euro, ExpandLess, ExpandMore, Grading, Layers, LocalAtm, Logout, Science, Storage } from '@mui/icons-material';
+import { Divider, Paper, Box, Typography, List, ListItemButton, ListItemIcon, ListItemText, Collapse } from '@mui/material';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const NavSide = () => {
+    /*********
+     * STATES
+     *********/
+    const { user } = useSelector((store) => store.auth);
+    const [openDaytrade, setOpenDaytrade] = useState(false);
+
     return (
         <Paper className={styles.nav_container}>
-            <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <img className={styles.nav_logo} src={require('../../../assets/back/img/logo.png')} alt='' />
+            <Box className={styles.nav_header}>
+                <img className={styles.nav_logo} src={require('../../../assets/back/img/mascot.png')} alt='' />
+                <div className={styles.nav_user}>
+                    <Typography variant='button'>Bom dia</Typography>
+                    <Typography className={styles.nav_username} variant='overline'>
+                        {user.nome}
+                    </Typography>
+                </div>
             </Box>
-            <Divider sx={{ background: '#ebf0f5', mt: 2, mb: 3 }} />
-            <Button className={styles.nav_button} variant='text' fullWidth startIcon={<CandlestickChart />}>
-                Daytrade
-            </Button>
+            <Divider variant='middle' className={styles.nav_divider} sx={{ mb: 1 }} />
+            <List component='nav' sx={{ flexGrow: 1 }}>
+                <ListItemButton className={styles.nav_button} onClick={() => setOpenDaytrade(!openDaytrade)}>
+                    <ListItemIcon>
+                        <CandlestickChart />
+                    </ListItemIcon>
+                    <ListItemText primary='Daytrade' primaryTypographyProps={{ variant: 'overline' }} />
+                    {openDaytrade ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+                <Collapse in={openDaytrade} timeout='auto' unmountOnExit>
+                    <List component='div' disablePadding>
+                        <ListItemButton className={styles.nav_button} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                                <Analytics />
+                            </ListItemIcon>
+                            <ListItemText primary='Dashboard' primaryTypographyProps={{ variant: 'overline' }} />
+                        </ListItemButton>
+                        <ListItemButton className={styles.nav_button} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                                <Storage />
+                            </ListItemIcon>
+                            <ListItemText primary='Datasets' primaryTypographyProps={{ variant: 'overline' }} />
+                        </ListItemButton>
+                        <ListItemButton className={styles.nav_button} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                                <Euro />
+                            </ListItemIcon>
+                            <ListItemText primary='Ativos' primaryTypographyProps={{ variant: 'overline' }} />
+                        </ListItemButton>
+                        <ListItemButton className={styles.nav_button} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                                <LocalAtm />
+                            </ListItemIcon>
+                            <ListItemText primary='Gerenciamentos' primaryTypographyProps={{ variant: 'overline' }} />
+                        </ListItemButton>
+                        <ListItemButton className={styles.nav_button} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                                <Grading />
+                            </ListItemIcon>
+                            <ListItemText primary='Cenarios' primaryTypographyProps={{ variant: 'overline' }} />
+                        </ListItemButton>
+                        <ListItemButton className={styles.nav_button} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                                <Science />
+                            </ListItemIcon>
+                            <ListItemText primary='Builds' primaryTypographyProps={{ variant: 'overline' }} />
+                        </ListItemButton>
+                        <ListItemButton className={styles.nav_button} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                                <Layers />
+                            </ListItemIcon>
+                            <ListItemText primary='Adiconar Operações' primaryTypographyProps={{ variant: 'overline' }} />
+                        </ListItemButton>
+                    </List>
+                </Collapse>
+                <ListItemButton className={styles.nav_button} sx={{ marginTop: 'auto' }}>
+                    <ListItemIcon>
+                        <Logout />
+                    </ListItemIcon>
+                    <ListItemText primary='Logout' primaryTypographyProps={{ variant: 'overline' }} />
+                </ListItemButton>
+            </List>
         </Paper>
     );
 };
--- src/pages/back/daytrade/Dashboard/index.js
+++ src/pages/back/daytrade/Dashboard/index.js
@@ -3,42 +3,39 @@ import React from 'react';
 import { Box, Grid } from '@mui/material';
 import NavSide from '../../../../components/back/NavSide';
 import { motion } from 'framer-motion';
-import { Container } from '@mui/system';
 
 const Home = () => {
     const consts = {
-        pageWidth_max: '1920px',
+        pageWidth_max: '2560px',
     };
 
     return (
-        <Box width='100%' height='100vh' sx={{ backgroundColor: '#ebf0f5' }}>
-            <Container sx={{ maxWidth: `${consts.pageWidth_max}` }} maxWidth={false}>
-                <Grid container spacing={2} sx={{ height: '100%' }}>
-                    <Grid item xs={2}>
-                        <Box
-                            sx={{ margin: '1.5rem' }}
-                            component={motion.div}
-                            initial={{ x: `-${consts.appbar_width}px` }}
-                            animate={{ x: 0, transition: { duration: 0.25 } }}
-                            exit={{ transition: { duration: 0.1 } }}
-                        >
-                            <NavSide />
-                        </Box>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Box
-                            sx={{ margin: '1.5rem' }}
-                            component={motion.div}
-                            initial={{ y: '-100vh' }}
-                            animate={{ y: 0, transition: { duration: 0.25 } }}
-                            exit={{ transition: { duration: 0.1 } }}
-                        >
-                            {'Content'}
-                        </Box>
-                    </Grid>
+        <div className={styles.wrapper}>
+            <Grid container spacing={2} sx={{ height: '100%', m: 0, width: '100%' }}>
+                <Grid item xs={2}>
+                    <Box
+                        sx={{ height: '100%' }}
+                        component={motion.div}
+                        initial={{ x: `-${consts.appbar_width}px` }}
+                        animate={{ x: 0, transition: { duration: 0.25 } }}
+                        exit={{ transition: { duration: 0.1 } }}
+                    >
+                        <NavSide />
+                    </Box>
                 </Grid>
-            </Container>
-        </Box>
+                <Grid item xs={10}>
+                    <Box
+                        sx={{ height: '100%' }}
+                        component={motion.div}
+                        initial={{ y: '-100vh' }}
+                        animate={{ y: 0, transition: { duration: 0.25 } }}
+                        exit={{ transition: { duration: 0.1 } }}
+                    >
+                        {'Content'}
+                    </Box>
+                </Grid>
+            </Grid>
+        </div>
     );
 };
 
