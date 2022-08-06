ccc2513505a3d984591d84a8ffad16108ef01e92
lfsc09 <lfsc09@gmail.com>
2022-08-06 16:30:15 -0300

    0.0.0.12
    Limpando dependencias

--- ckeditor5/build/translations/oc.js
+++
@@ -1,1 +0,0 @@
-(function(o){const i=o["oc"]=o["oc"]||{};i.dictionary=Object.assign(i.dictionary||{},{"%0 of %1":"",Bold:"Gras",Code:"",Italic:"Italica","Remove color":"","Show more items":"",Strikethrough:"",Subscript:"",Superscript:"",Underline:""});i.getPluralForm=function(o){return o>1}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
